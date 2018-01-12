5ac5604f98b48e207e1e668cf75a283da34dd311
shijieluo <shjluo@zju.edu.cn>
2018-01-12 23:07:30 +0800

    modify

--- JudgeRouteCircle.cpp
+++ JudgeRouteCircle.cpp
@@ -1,45 +1,51 @@
-class Point;
+class Pointt{
+    public:
+    int x,y;
+    Pointt(int x=0,int y=0):x(x),y(y){}
+    Pointt operator+(const Pointt &a);
+    bool operator==(const Pointt &a);
+   // Pointt operator=(const Pointt &a);
+};
+
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        Point *a=new Point();        
-        map<char,Point> m;
-        m['U']=Point(0,1);
-        m['R']=Point(1,0);
-        m['D']=Point(0,-1);
-        m['L']=Point(-1,0);       
+        Pointt *a=new Pointt(0,0);
+        map<char,Pointt> m;
+        m['U']=Pointt(0,1);
+        m['R']=Pointt(1,0);
+        m['D']=Pointt(0,-1);
+        m['L']=Pointt(-1,0);
         for(int i=0;i<moves.size();i++){
-            for(map<char,Point>::iterator it=m.begin(); it!=m.end(); ++it){
+            for(map<char,Pointt>::iterator it=m.begin(); it!=m.end(); ++it){
                 if(moves[i] == it->first){
                     *a = *a + it->second;
                     break;
                 }
-            }   
+            }
         }
-        if(*a == Point(0,0))
+        if(*a == Pointt(0,0))
             return true;
         else
             return false;
     }
 };
 
-class Point{
-    public:
-    int x,y;    
-    Point(int x=0,int y=0):x(x),y(y){}
-    Point operator+(const Point &a,const Point &b);
-    bool operator==(const Point &a);
-}
-Point Point::operator+(const Point &a,const Point &b){
-    Point *res=new Point();
-    res->x = a.x + b.x;
-    res->y = a.y + b.y;
-    return *res;
+Pointt Pointt::operator+(const Pointt &a){
+    this->x = this->x + a.x;
+    this->y = this->y + a.y;
+    return *this;
 }
 
-bool operator==(const Point &a){
-    if(this->x == a.x && this->y == a.y){
+bool Pointt::operator==(const Pointt &b){
+    if( this->x == b.x && this->y == b.y){
         return true;
     }
     else return false;
 }
+/*
+Pointt Pointt::operator=(const Pointt &a){
+    this->x = a.x;
+    this->y = a.y;
+    return *this;
+}*/
